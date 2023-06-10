3df8a4933323eae06503cbbd125a09536deea95b
ellidey <ellidey00@gmail.com>
2023-06-10 20:13:04 +0700

    Change students system

--- src/renderer/views/students/StudentEdit.vue
+++ src/renderer/views/students/StudentEdit.vue
@@ -6,7 +6,9 @@ export default Vue.extend({
   name: 'StudentAdd',
   data: function () {
     return {
-      student: {},
+      student: {
+        parents: []
+      },
       errors: {}
     }
   },
@@ -28,9 +30,6 @@ export default Vue.extend({
       }
       if (!this.student.group_id) {
         this.errors.group_id = 'Введите группу';
-      }
-      if (!this.student.parent) {
-        this.errors.parent = 'Введите ФИО родителя студента';
       }
 
       if (Object.keys(this.errors).length > 0) {
@@ -60,10 +59,63 @@ export default Vue.extend({
       </div>
 
       <div class="mb-5">
+        <label class="label">Паспорт</label>
+        <input class="input" type="text" placeholder="Введите паспорт" v-model="student.passport">
+        <p class="help is-danger" v-if="errors.passport">{{ errors.passport }}</p>
+      </div>
+
+      <div class="mb-5">
+        <label class="label">Телефон</label>
+        <input class="input" type="text" placeholder="Введите телефон" v-model="student.phone">
+        <p class="help is-danger" v-if="errors.phone">{{ errors.phone }}</p>
+      </div>
+
+      <div class="mb-5">
+        <label class="label">Дата рождения</label>
+        <input class="input" type="text" placeholder="Введите дату рождения" v-model="student.date">
+        <p class="help is-danger" v-if="errors.date">{{ errors.date }}</p>
+      </div>
+
+      <div class="mb-5">
+        <label class="label">Снилс</label>
+        <input class="input" type="text" placeholder="Введите Снилс" v-model="student.snils">
+        <p class="help is-danger" v-if="errors.snils">{{ errors.snils }}</p>
+      </div>
+
+      <div class="mb-5">
+        <label class="label">Пол</label>
+        <div class="select is-small">
+          <select v-model="student.gender">
+            <option selected value="М">Мужской</option>
+            <option selected value="Ж">Женский</option>
+          </select>
+        </div>
+        <p class="help is-danger" v-if="errors.group_id">{{ errors.group_id }}</p>
+      </div>
+
+      <div class="mb-5">
+        <label class="label">Учет</label>
+        <input class="input" type="text" placeholder="Введите учет" v-model="student.data">
+        <p class="help is-danger" v-if="errors.data">{{ errors.data }}</p>
+      </div>
+
+      <div class="mb-5">
+        <label class="label">Место прописки</label>
+        <input class="input" type="text" placeholder="Введите место прописки" v-model="student.place">
+        <p class="help is-danger" v-if="errors.place">{{ errors.place }}</p>
+      </div>
+
+      <div class="mb-5">
+        <label class="label">Место жительства</label>
+        <input class="input" type="text" placeholder="Введите место жительства" v-model="student.place_live">
+        <p class="help is-danger" v-if="errors.place_live">{{ errors.place_live }}</p>
+      </div>
+
+      <div class="mb-5">
         <label class="label">Группа</label>
         <div class="select is-small">
           <select v-model="student.group_id">
-            <option selected value="">Выберите группу</option>
+            <option disabled selected value="">Выберите группу</option>
             <option v-for="group in getGroups" :key="group.id" :value="group.id">
               {{ group.name }}
             </option>
@@ -72,10 +124,61 @@ export default Vue.extend({
         <p class="help is-danger" v-if="errors.group_id">{{ errors.group_id }}</p>
       </div>
 
-      <div class="mb-5">
-        <label class="label">Родитель</label>
-        <input class="input" type="text" placeholder="Введите ФИО родителя студента" v-model="student.parent">
-        <p class="help is-danger" v-if="errors.parent">{{ errors.parent }}</p>
+      <h3>Родители</h3>
+
+      <div class="mb-5" style="border: 1px solid rgba(0, 0, 0, 0.5); padding: 10px" v-for="(parent, index) in student.parents" :key="index">
+        <div class="columns">
+          <div class="column">
+            <label class="label">Родитель</label>
+            <div class="select is-fullwidth is-small" style="height: 35px">
+              <select v-model="parent.type" style="height: 35px">
+                <option selected value="1">Мама</option>
+                <option value="2">Папа</option>
+                <option value="3">Опекун</option>
+              </select>
+            </div>
+          </div>
+          <div class="column">
+            <label class="label">Телефон</label>
+            <input class="input" type="text" placeholder="Введите телефон" v-model="parent.phone">
+          </div>
+        </div>
+
+        <div class="mb-5">
+          <label class="label">ФИО</label>
+          <input class="input" type="text" placeholder="Введите ФИО" v-model="parent.name">
+        </div>
+
+        <div class="mb-5">
+          <label class="label">Образование</label>
+          <input class="input" type="text" placeholder="Введите образование" v-model="parent.education">
+        </div>
+
+        <div class="mb-1">
+          <label class="label">Место работы</label>
+          <input class="input" type="text" placeholder="Введите место работы" v-model="parent.job">
+        </div>
+
+        <div style="text-align: right">
+          <button class="button is-small is-danger"
+                  style="height: 35px;"
+                  @click="student.parents = student.parents.filter(pr => {
+                 return pr !== parent;
+              })"
+          >
+        <span class="icon is-small">
+          <i class="ri-delete-bin-2-fill"></i>
+        </span>
+          </button>
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <button class="button is-small" @click="student.parents.push({})">
+      <span class="icon is-small">
+        <i class="ri-add-fill"></i>
+      </span>
+        </button>
       </div>
 
       <button class="button is-success" @click="save">Сохранить</button>
--- src/renderer/views/students/StudentIndex.vue
+++ src/renderer/views/students/StudentIndex.vue
@@ -7,11 +7,27 @@ export default Vue.extend({
   name: 'Students',
   components: { Popper },
   computed: {
-    ...mapGetters(['getStudents']),
+    ...mapGetters(['getStudents', 'getGroups']),
+    filterStudents() {
+      const students = this.getStudents;
+      if (this.group_id) {
+        return students.filter(student => {
+          return student.group_id === this.group_id;
+        })
+      }
+
+      return students;
+    }
   },
   data: function () {
     return {
-
+      selectStudent: null,
+      group_id: null,
+      parentTypes: {
+        1: 'Мама',
+        2: 'Папа',
+        3: 'Опекун'
+      }
     }
   },
   methods: {
@@ -25,9 +41,20 @@ export default Vue.extend({
   <div>
     <h1>Список студентов</h1>
     <div class="box">
-      <router-link tag="button" to="/students/add" class="button is-primary mb-4">
-        Добавить
-      </router-link>
+      <div style="display: flex; gap: 5px;">
+        <router-link tag="button" to="/students/add" class="button is-primary mb-4">
+          Добавить
+        </router-link>
+
+        <div class="select is-small">
+          <select v-model="group_id" style="width: 200px; height: 35px;">
+            <option selected value="">Все</option>
+            <option v-for="group in getGroups" :key="group.id" :value="group.id">
+              {{ group.name }}
+            </option>
+          </select>
+        </div>
+      </div>
 
       <article class="message is-success mt-4" v-if="getStudents.length === 0">
         <div class="message-body">
@@ -41,17 +68,37 @@ export default Vue.extend({
         <tr>
           <th>ФИО</th>
           <th>Возраст</th>
+          <th>Пол</th>
           <th>Группа</th>
-          <th>Родитель</th>
+          <th>Телефон</th>
+          <th>Дата рождения</th>
+          <th>Паспорт</th>
+          <th>Снилс</th>
+          <th>Учет</th>
+          <th>Место прописки</th>
+          <th>Место жительства</th>
+          <th>Родители</th>
           <th></th>
         </tr>
         </thead>
         <tbody>
-        <tr v-for="student in getStudents" :key="student.id">
-          <td>{{ student.name }}</td>
-          <td>{{ student.age }}</td>
+        <tr v-for="student in filterStudents" :key="student.id">
+          <td>{{ student.name || '-' }}</td>
+          <td>{{ student.age || '-' }}</td>
+          <td>{{ student.gender || '-' }}</td>
           <td>{{ $store.getters.getGroup(student.group_id)?.name || 'Неизвестно' }}</td>
-          <td>{{ student.parent }}</td>
+          <td>{{ student.phone || '-' }}</td>
+          <td>{{ student.date || '-' }}</td>
+          <td>{{ student.passport || '-' }}</td>
+          <td>{{ student.snils || '-' }}</td>
+          <td>{{ student.data || '-' }}</td>
+          <td>{{ student.place || '-' }}</td>
+          <td>{{ student.place_live || '-' }}</td>
+          <td>
+            <button @click="selectStudent = student" class="button is-small is-primary" style="padding: 0 10px;">
+              <i class="ri-eye-2-fill"></i>
+            </button>
+          </td>
           <td>
             <popper
               trigger="clickToOpen"
@@ -79,6 +126,45 @@ export default Vue.extend({
         </tr>
         </tbody>
       </table>
+    </div>
+
+    <div class="modal" :class="{ 'is-active': selectStudent }">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Родители {{ selectStudent?.name }}</p>
+          <button @click="selectStudent = null" class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <article class="message is-success mt-4" v-if="selectStudent?.parents?.length === 0">
+            <div class="message-body">
+              <b>Родители не найдены</b><br>
+              Пожалуйста, добавьте родителей
+            </div>
+          </article>
+
+          <table class="table is-fullwidth" v-else>
+            <thead>
+            <tr>
+              <th>Родитель</th>
+              <th>ФИО</th>
+              <th>Телефон</th>
+              <th>Образование</th>
+              <th>Место работы</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(parent, index) in selectStudent?.parents" :key="index">
+              <td>{{ parentTypes[parent.type] || '-' }}</td>
+              <td>{{ parent.name || '-' }}</td>
+              <td>{{ parent.phone || '-' }}</td>
+              <td>{{ parent.education || '-' }}</td>
+              <td>{{ parent.job || '-' }}</td>
+            </tr>
+            </tbody>
+          </table>
+        </section>
+      </div>
     </div>
   </div>
 </template>
